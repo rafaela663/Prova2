eb1d3be7fbfe69c175c346721557c0dd23beff35
rafaela663 <rafaelarodrigues8055@gmail.com>
2023-06-20 10:31:46 -0300

    prova2

--- conexao.php
+++ conexao.php
@@ -1,13 +1,15 @@
 <?php
-    $hostname = 'localhost';
-    $username = 'root';
-    $password = 'rafinha';
-    $database = 'prova2';
-    $port = 3307; 
-    $con = mysqli_connect($hostname, $username, $password, $database, $port);
-    if(mysqli_connect_errno()){
-        printf("Erro na conexão: %s\n",
-            mysqli_connect_error());
-            exit();
-    }
+$hostname = "localhost";
+$username = "root";
+$password = "";
+$database = "prova2";
+$port = 3307;
+$con = mysqli_connect($hostname, $username, $password, $database, $port); 
+
+if (mysqli_connect_error()) {
+    printf("Erro conexão: %s\n", mysqli_connect_error());
+
+    exit(); 
+}
+
 ?>
