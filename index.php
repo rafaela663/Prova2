85b4c2ebbfb8745b08bf4c25d9bbfe14ba2467f4
rafaela663 <rafaelarodrigues8055@gmail.com>
2023-06-20 08:49:34 -0300

    prova2

--- index.php
+++ index.php
@@ -11,10 +11,11 @@
     <title>Document</title>
 </head>
 <body>
-    <h3>Página Inicial - Projeto Cadastro IFSP</h3>
+    <h1>PÁGINA INICIAL - PROJETO CAIXA</h1>
     <?php 
-        if(!empty($_SESSION['login'])){ 
-            echo "<h3>Ola ".$_SESSION['login']['nome_usuario']."</h3>"; 
+        if(!empty($_SESSION['login'])){ // caso a sessão esteja diferente de vazia
+            // executa isso.
+            echo "<h3>Ola ".$_SESSION['login']['nome_usuario']."</h3>"; // pega o nome do login
             echo "<a href='logout.php'>Sair</a>";
         }
     ?>
@@ -24,9 +25,7 @@
        <li><a href="listar_fluxo_caixa.php">Listar</a></li>
        <li><a href="consulta_fluxo_caixa.php">Consultar</a></li>
        <?php 
-       if(empty($_SESSION['login'])){  
-       echo "<li><a href='login.html'>Login</a></li>";
-        }
+       
        ?>
     </ul>
 </body>
